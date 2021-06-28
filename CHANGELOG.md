29a32dd4036caef50801fa68bda57f87dc1df48f
egor-lukyanov <elukianov@isgneuro.com>
2021-06-28 18:39:25 +0300

    feat: csv separator

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.2.4] - 2021-06-28
+### Fixed
+- CSV formatting issue
+### Added
+- CSV separator configurable
+
 ## [0.2.3] - 2021-06-08
 ### Fixed
 - Default config file path with system-wide installation
--- lib_clisearch/dataset_converter/__init__.py
+++ lib_clisearch/dataset_converter/__init__.py
@@ -12,15 +12,15 @@ class DatasetConverter:
         self.logger = logging.getLogger(__name__)
         self.logger.debug("Creating DatasetConverter instance")
 
-    def get_csv(self):
-        return CSV.convert(self.dataset)
+    def get_csv(self, separator=','):
+        return CSV.convert(self.dataset, separator)
 
     def get_json(self):
         return JSON.convert(self.dataset)
 
-    def print_csv(self):
+    def print_csv(self, separator):
         self.logger.debug("Printing dataset as CSV")
-        for row in self.get_csv():
+        for row in self.get_csv(separator):
             sys.stdout.write(row)
 
     def print_json(self):
